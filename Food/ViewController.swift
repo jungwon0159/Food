feb560a2e93fa0c48956c77852f6079434efadec
joongwon <wnddnjs823@ajou.ac.kr>
2022-07-08 09:34:40 +0900

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by 이중원 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        print(1)
+        print(2)
+        print(3)
+        
+        print(4)
+        print(5)
+    }
+
+
+}
+
